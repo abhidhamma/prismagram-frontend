108e4dfc8c4388288b63712d638278032f0b78f1
hanji <abhidhamma91@gmail.com>
2019-12-11 13:06:25 +0900

    Updates

---
+++ precache-manifest.ad31ce739e21a1a8d33e1dd299a3c49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f8ec30db273d5ca3d950901b17c931",
+    "url": "abhidhamma.github.io/prismagram-frontend/index.html"
+  },
+  {
+    "revision": "d9dff26292fd598d5c89",
+    "url": "abhidhamma.github.io/prismagram-frontend/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "d9dff26292fd598d5c89",
+    "url": "abhidhamma.github.io/prismagram-frontend/static/js/2.e8d4ac71.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "abhidhamma.github.io/prismagram-frontend/static/js/2.e8d4ac71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ea37891479e9f83929f",
+    "url": "abhidhamma.github.io/prismagram-frontend/static/js/main.8a9f5173.chunk.js"
+  },
+  {
+    "revision": "5f45304fee6fef9e3dfb",
+    "url": "abhidhamma.github.io/prismagram-frontend/static/js/runtime-main.4ae20ab9.js"
+  }
+]);
